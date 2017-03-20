5d3689f3a42b6b19d33dbc7e0460e07464d0d698
umlstudy@gmail.com <umlstudy@gmail.com>
2017-03-20 14:43:25 +0900

    ....

---
+++ asia.sejong.eclipse.plugin.tools/postbuild.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf  /data/important/www/html/files/target
+cp -r /data/home/jenkins/.jenkins/jobs/eclipse_plugin_tools/workspace/asia.sejong.eclipse.plugin.tools.updatesite/target  /data/important/www/html/files/
